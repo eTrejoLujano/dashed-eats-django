1ad7a373a8296636ae790e459522e723624bbefe
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-05-29 12:10:33 -0700

    initating jwt tokens

--- instadash/urls.py
+++ instadash/urls.py
@@ -17,8 +17,19 @@ from django.contrib import admin
 from django.urls import path
 from instadash import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('api/users/', views.users, name="users"),
-    path('api/ads/', views.ads, name="ads")
+    path('api/ads/', views.ads, name="ads"),
+    path('api/routes/', views.getRoutes, name='routes')
 ]
--- instadash/views.py
+++ instadash/views.py
@@ -1,6 +1,17 @@
 from instadash.models import User, Ad
 from django.http import JsonResponse
 from instadash.serializers import UserSerializer, AdSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh'
+    ]
+    return Response(routes)
 
 
 def users(request):
