17d2c6b49c2ff8444b0e69bd8bd0a7909211d20a
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-06-06 23:32:35 -0700

    requirement txt updated, token serializer implemented

--- instadash/urls.py
+++ instadash/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from instadash import views
+from .views import MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -26,7 +26,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('api/users/', views.users, name="users"),
