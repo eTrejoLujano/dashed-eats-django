a496b7b7d8a23ac4b1ce9018ce6f643823036522
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-07-13 13:00:24 -0700

    testing

--- instadash/settings.py
+++ instadash/settings.py
@@ -26,7 +26,7 @@ AUTH_USER_MODEL = "instadash.User"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
