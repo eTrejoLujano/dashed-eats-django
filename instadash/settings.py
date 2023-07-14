567f8cd10ca7cb98cb0290c0b67c91de3a281fa2
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-07-13 23:11:31 -0700

    mimetypes html

--- instadash/settings.py
+++ instadash/settings.py
@@ -21,6 +21,7 @@ import mimetypes
 
 # mimetypes.add_type("text/javascript", ".js", True)
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 
 from pathlib import Path
