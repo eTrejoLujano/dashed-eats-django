6333693aea89f9d561e869fb87d5c4139bd4a5eb
Erik Trejo Lujano <trejolujano.erik@gmail.com>
2023-10-23 01:02:38 -0700

    removed 2 allowed hosts

--- instadash/settings.py
+++ instadash/settings.py
@@ -51,7 +51,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
 
 ALLOWED_HOSTS = ['dashedeatsdjango.us-west-2.elasticbeanstalk.com',
-                 'react.dashedeatsdjango.com', '127.0.0.1', '10.0.23.180', '52.35.26.201']
+                 'react.dashedeatsdjango.com', '127.0.0.1']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
